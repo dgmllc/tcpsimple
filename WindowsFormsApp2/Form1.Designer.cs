795e2b67c8a095817a8663216ecc0ac6485dfbdf
David Deininger <ddeininger@griffinmotion.com>
2019-01-04 15:00:34 -0500

    initial

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,13 +28,222 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ip_connect = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.ip_send = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "192.168.15.165";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PORT";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ip_connect
+            // 
+            this.ip_connect.Location = new System.Drawing.Point(289, 84);
+            this.ip_connect.Name = "ip_connect";
+            this.ip_connect.Size = new System.Drawing.Size(75, 64);
+            this.ip_connect.TabIndex = 2;
+            this.ip_connect.Text = "CONNECT";
+            this.ip_connect.UseVisualStyleBackColor = true;
+            this.ip_connect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "IP ADDRESS";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "8888";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox3.Location = new System.Drawing.Point(126, 363);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(122, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // ip_send
+            // 
+            this.ip_send.Location = new System.Drawing.Point(289, 319);
+            this.ip_send.Name = "ip_send";
+            this.ip_send.Size = new System.Drawing.Size(75, 64);
+            this.ip_send.TabIndex = 6;
+            this.ip_send.Text = "SEND";
+            this.ip_send.UseVisualStyleBackColor = true;
+            this.ip_send.Click += new System.EventHandler(this.ip_send_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(126, 319);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(122, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "hi";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "RESPONSE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 322);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "COMMAND";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox5.Location = new System.Drawing.Point(161, 179);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(203, 20);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "CONNECTION STATUS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(143, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "CONNECT";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(106, 263);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(181, 25);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "COMMUNICATE";
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox6.Location = new System.Drawing.Point(34, 487);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(330, 20);
+            this.textBox6.TabIndex = 14;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(156, 441);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 25);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "DEBUG";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(406, 537);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.ip_send);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ip_connect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ip_connect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button ip_send;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -6,15 +6,147 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;           //for TcpClient
+using System.Net;                   //for IPEndPoint
+using System.Threading;             //for thread creation
 using System.Windows.Forms;
 
 namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+
+        //required globals for TCP communication
+        private static TcpClient tcpClient = new TcpClient();
+        private static NetworkStream stream;
+        private static bool started = false;
+        private static byte[] msg_rcv = new byte[1024];
+
+        //delegate for talking between threads
+        public delegate void textBox3_update_d(string passed_string);
+
+        //Initialize the UI (look in Main.cs)
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //connect to TCP Host
+        private void connect(IPEndPoint pointer) {
+            tcpClient.Connect(pointer);
+            stream = tcpClient.GetStream();
+            Thread t = new Thread(new ThreadStart(read));
+            started = true;
+            t.Start();
+            textBox6.Text = "Started Thread";
+        }
+
+        //send a message to TCP host (requires connection to be already established)
+        private void send(string outbound) {
+            byte[] bytes = Encoding.ASCII.GetBytes(outbound);
+            int size = bytes.Length;
+            stream.Write(bytes,0,size);
+        }
+
+        //read a message from TCP host
+        //this is started by connect() as a separate continuously running thread
+        private void read() {
+            //loop continously forever
+            while (started) {
+                //check receive buffer for a message
+                if (tcpClient.ReceiveBufferSize > 0) {
+                    //allocate buffer size
+                    msg_rcv = new byte[tcpClient.ReceiveBufferSize];
+                    //use stream objecto read in the bytes[]
+                    stream.Read(msg_rcv, 0, tcpClient.ReceiveBufferSize);
+                    //conver to string for printing
+                    string msg = Encoding.ASCII.GetString(msg_rcv);
+                    //delegate pointer to a thread which already exists
+                    //this is required because this thread doesn't know where textBox3.Text lives.
+                    textBox3.Invoke(new textBox3_update_d(this.textBox3_update), new object[] { msg });
+                }
+                //just sleep arbitrarily to prevent overrun?
+                Thread.Sleep(1000);
+            }
+        }
+
+        //used as a buffer between textBox3.text and read() for printing the received TCP message
+        private void textBox3_update(string passed_string) {
+            textBox3.Text = passed_string;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //ip address input, do nothing here
+        }
+
+        //Grab UI values and attempt to connect to the server IP and Port
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = "IP Setup";
+
+            //attemp to connect to controller
+            string ip_add_t = textBox1.Text;
+            string ip_port_t = textBox2.Text;
+            int ip_port = Int32.Parse(ip_port_t);
+            string ip_connect = ip_add_t + ":" + ip_port;
+            textBox5.Text = ip_connect;
+
+            //Uses a remote endpoint to establish a socket connection.
+            IPAddress ipAddress = IPAddress.Parse(ip_add_t);
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, ip_port);
+
+            //doesn't need to be in a separate method, but I did it anyway
+            connect(ipEndPoint);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //do nothing
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            //do nothing
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            //do nothing
+        }
+
+        private void ip_send_Click(object sender, EventArgs e)
+        {
+            //attempt to send a message
+            textBox6.Text = "Message Sent";
+            string ip_msg = textBox4.Text;
+            //doesn't need to be in another method, but I did it anyway
+            send(ip_msg);
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            //status update on connection, do nothing here
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //port address input, do nothing here
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            //do nothing here
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            //response input from controller, do nothing here
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            //where debug information gets printed, do nothing here
         }
     }
 }
